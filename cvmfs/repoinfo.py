7723123dd7e39695ffdf00318e0015b664a14455
Rene Meusel <rene@renemeusel.de>
2016-01-13 17:23:23 +0100

    FIX: add pretty-printers for RepoInfo

--- cvmfs/repoinfo.py
+++ cvmfs/repoinfo.py
@@ -10,13 +10,23 @@ import json
 class RepoInfo:
     """ Wraps JSON repository information contained in a CernVM-FS repo """
 
-    def __init__(self, repo_name, repo_info_file):
+    def __init__(self, fqrn, repo_info_file):
+        self.fqrn = fqrn
         self.json_data = json.load(repo_info_file)
         self.__extract_if_exists("email")
         self.__extract_if_exists("administrator")
         self.__extract_if_exists("organisation")
         self.__extract_if_exists("description")
         self.__extract_if_exists("recommended-stratum1s", "stratum1s")
+
+
+    def __str__(self):
+        return "<RepoInfo for " + self.fqrn + ">"
+
+
+    def __repr__(self):
+        return self.__str__()
+
 
     def __extract_if_exists(self, field, member_name = ""):
         if not member_name:
